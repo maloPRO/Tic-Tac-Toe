3b46e408d7573f6b9db08f7f8588ac5031fded75
maloPRO <malovagilbert@gmail.com>
2022-10-20 11:36:17 +0300

    Added modules and create player factory functions

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+const gameBoard = (function () {
+  let board = [];
+
+  return {}
+
+})();
+
+const player = (name, selelection) => {
+    
+}
+
+const displayController = (function() {
+	
+})();
+
+const game = (function() {
+
+})();
