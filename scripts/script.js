5b810cbe52b9dc1125eb57e926197ebbb8fb9d88
maloPRO <malovagilbert@gmail.com>
2022-11-30 21:54:54 +0300

    changed tab size from 4 to 2

--- scripts/script.js
+++ scripts/script.js
@@ -1,38 +1,36 @@
 const gameBoard = (function () {
-    const board = ["","","","","","","","",""];
-    const gameBoardContainer = document.getElementById('gameBoardContainer');
+  const board = ["","","","","","","","",""];
+  const gameBoardContainer = document.getElementById('gameBoardContainer');
 
-    board.forEach((boardItem) => {
-        const item = document.createElement('div');
-        item.setAttribute('class', 'boardItems');
-        gameBoardContainer.appendChild(item);
-        
-    })
+  board.forEach((boardItem) => {
+    const item = document.createElement('div');
+    item.setAttribute('class', 'boardItems');
+      gameBoardContainer.appendChild(item);    
+  })
 
-    return {}
+  return {}
 })();
 
 const player = (playerName, num) => {
-    const getName = () => playerName;
+  const getName = () => playerName;
 
-    const getMark = () => {
-        if (num === 1) {
-            return 'X';
-        } else if (num === 2) {
-            return 'O';
-        }
-    };
+  const getMark = () => {
+    if (num === 1) {
+	    return 'X';
+    } else if (num === 2) {
+      return 'O';
+    }
+  };
 
-    const getNum = () => num
+  const getNum = () => num
 
-    return { playerName, getMark, getName, getNum }
+  return { playerName, getMark, getName, getNum }
 }
 
 const displayController = (function () {
-    const items = document.querySelectorAll('.boardItems');
-    items.forEach((item) => {
-        item.addEventListener('click', function () {
-            
-        })
-    })
+  const items = document.querySelectorAll('.boardItems');
+  items.forEach((item) => {
+    item.addEventListener('click', function () {        
+      })
+  })
 })();
