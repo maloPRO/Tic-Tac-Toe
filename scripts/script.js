f17d70d22ce28cddbf4faa287b89eb471d0c3cb6
maloPRO <malovagilbert@gmail.com>
2022-11-30 21:52:19 +0300

    added board and add player function

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,38 @@
 const gameBoard = (function () {
-  let board = [];
+    const board = ["","","","","","","","",""];
+    const gameBoardContainer = document.getElementById('gameBoardContainer');
 
-  return {}
+    board.forEach((boardItem) => {
+        const item = document.createElement('div');
+        item.setAttribute('class', 'boardItems');
+        gameBoardContainer.appendChild(item);
+        
+    })
 
+    return {}
 })();
 
-const player = (name, selelection) => {
-    
+const player = (playerName, num) => {
+    const getName = () => playerName;
+
+    const getMark = () => {
+        if (num === 1) {
+            return 'X';
+        } else if (num === 2) {
+            return 'O';
+        }
+    };
+
+    const getNum = () => num
+
+    return { playerName, getMark, getName, getNum }
 }
 
-const displayController = (function() {
-	
-})();
-
-const game = (function() {
-
+const displayController = (function () {
+    const items = document.querySelectorAll('.boardItems');
+    items.forEach((item) => {
+        item.addEventListener('click', function () {
+            
+        })
+    })
 })();
